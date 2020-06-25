5262e06b7d95ca24a0c087b883067e7b3b049679
Ashley Felton <ashley@ropable.com>
2020-06-25 14:31:02 +0800

    Add pytest-mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,33 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_suceeds(runner):
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch('requests.get')
+    mock.return_value.__enter__.return_value.json.return_value = {
+        'title': 'Lorem Ipsum',
+        'extract': 'Lorem ipsum dolor sit amet',
+    }
+    return mock
+
+
+def test_main_suceeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert 'Lorem Ipsum' in result.output
+
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert 'en.wikipedia.org' in args[0]
+
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception('Boom!')
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
